375649f31cf4cbe095348b6a0e9cb9fbea21a418
Glenn 'devalias' Grant <glenn@devalias.net>
2018-10-12 13:20:55 +1100

    Update README and code to manually wire PoC Subscription/Mutation into React

--- src/index.js
+++ src/index.js
@@ -4,6 +4,64 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from '@aws-amplify/core';
+import API, {graphqlOperation} from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub';
+
+// console.log("Automagicly Wired?", Amplify.API !== null);
+
+// Not 100% if we need to do this.. but it seems to get rid of the unused variable errors..
+Amplify.register(API);
+Amplify.register(PubSub);
+
+const aws_config = {
+  'aws_appsync_graphqlEndpoint': 'https://xxxxxx.appsync-api.us-east-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'us-east-1',
+  'aws_appsync_authenticationType': 'API_KEY',
+  'aws_appsync_apiKey': 'da2-xxxxxxxxxxxxxxxxxxxxxxxxxx',
+};
+
+Amplify.configure(aws_config);
+
+// Configure our mutation
+const SendCommand = `mutation SendCommand($channelID: ID!, $command: String!) {
+  sendCommand(channelID: $channelID, command: $command) {
+    channelID
+    command
+    sentAt
+  }
+}`;
+
+// Configure our subscription
+const SubscribeToChannelCommands = `subscription SubscribeToChannelCommands($channelID: ID!) {
+  receivedCommand(channelID: $channelID) {
+    channelID
+    command
+    sentAt
+  }
+}`;
+
+// Subscribe to channel
+const subscription = Amplify.API.graphql(
+  graphqlOperation(SubscribeToChannelCommands, { channelID: 'abc123' })
+).subscribe({
+  next: (eventData) => console.log("Subscription:", eventData, eventData.value.data),
+  error: (eventData) => console.log("Subscription error:", eventData)
+});
+
+// Send mutation after a short delay
+setTimeout(function(){
+  const sendCommand = Amplify.API.graphql(
+    graphqlOperation(SendCommand, { channelID: 'abc123', command: "FOOCOMMAND"})
+  ).then(
+    (result) => console.log("Mutation:", result),
+    (error) => console.log("Mutation error:", error)
+  );
+}, 1000);
+
+// Stop receiving data updates from the subscription
+//subscription.unsubscribe();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
